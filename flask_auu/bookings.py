f8d63f5316c4af68c3be7a4f2f57c57c30522fa9
Felix <muhaha501@hotmail.com>
2025-03-15 10:06:11 +0100

    Diverse Aufräumarbeiten

--- flask_auu/bookings.py
+++ flask_auu/bookings.py
@@ -52,37 +52,54 @@ def bookings():
 @login_required
 def new_booking():
     db = get_db()
-    abfahrtshafen_liste = db.execute("Select DISTINCT Abfahrtshafen from Passage").fetchall()
-    abfahrtshafen_liste = [hafen['Abfahrtshafen'] for hafen in abfahrtshafen_liste]
-    zielhafen_liste = []
-    if request.method == 'POST':
-        pressed_button = request.form.get('button_submit')
-        abfahrtshafen = request.form['abfahrtshafen']
-        zielhafen = request.form['zielhafen']
-        date = request.form['date']
-        if abfahrtshafen != "-- Wählen Sie einen Abfahrtshafen --":
-            zielhafen_liste = db.execute("Select DISTINCT Zielhafen from Passage where Abfahrtshafen = ?", (abfahrtshafen,)).fetchall()
-            zielhafen_liste = [hafen['Zielhafen'] for hafen in zielhafen_liste]
-        if pressed_button:
-            passage = db.execute("Select Passagennummer from Passage where Abfahrtshafen = ? and Zielhafen = ?", (abfahrtshafen,zielhafen,)).fetchone()['Passagennummer']
-            passpers = db.execute(
-                "SELECT Vorname, Nachname, strftime('%d.%m.%Y', Gebdatum) as Gebdatum, Passagiernr"
-                ' FROM Person, Passagier'
-                ' WHERE Person.PANr = Passagier.PANr and Person.PANr = ? ', (g.user['username'],)
-            ).fetchone()['PassagierNr']
-            buchungsnummer = db.execute("Select max(Buchungsnummer) as Buchungsnummer from Buchen").fetchone()['Buchungsnummer']
-            buchungsnummer = int(buchungsnummer[1:])+1
-            buchungsnummer = "B"+str(buchungsnummer).zfill(3)
-            db.execute(
-                'INSERT INTO Buchen (Buchungsnummer, Datum, Klasse, PassagierNr, Passagennummer)'
-                ' VALUES (?, ?, ?, ?, ?)',
-                (buchungsnummer, date, 1, passpers, passage)
-            )
-            db.commit()
-            return redirect(url_for('bookings.bookings'))
+    passpers = db.execute(
+            "SELECT Vorname, Nachname, strftime('%d.%m.%Y', Gebdatum) as Gebdatum, Passagiernr" 
+            ' FROM Person, Passagier'
+            ' WHERE Person.PANr = Passagier.PANr and Person.PANr = ? ', (g.user['username'],)
+    ).fetchone()
+    if passpers is not None:
+        abfahrtshafen_liste = db.execute("Select DISTINCT Abfahrtshafen from Passage").fetchall()
+        abfahrtshafen_liste = [hafen['Abfahrtshafen'] for hafen in abfahrtshafen_liste]
+        zielhafen_liste = []
+        if request.method == 'POST':
+            pressed_button = request.form.get('button_submit')
+            abfahrtshafen = request.form['abfahrtshafen']
+            zielhafen = request.form['zielhafen']
+            date = request.form['date']
+            print("Datum:" + date)
+            if abfahrtshafen != "-- Wählen Sie einen Abfahrtshafen --":
+                zielhafen_liste = db.execute("Select DISTINCT Zielhafen from Passage where Abfahrtshafen = ?", (abfahrtshafen,)).fetchall()
+                zielhafen_liste = [hafen['Zielhafen'] for hafen in zielhafen_liste]
+            if pressed_button:
+
+                today = datetime.date.today()
+                date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+
+                if date < today:
+                    flash('The date is in the past. Please select a future date.', category='danger')
+                    return redirect(url_for('bookings.new_booking'))
+                klasse = request.form['klasse']
+                passage = db.execute("Select Passagennummer from Passage where Abfahrtshafen = ? and Zielhafen = ?", (abfahrtshafen,zielhafen,)).fetchone()['Passagennummer']
+                passpers = db.execute(
+                    "SELECT Vorname, Nachname, strftime('%d.%m.%Y', Gebdatum) as Gebdatum, Passagiernr"
+                    ' FROM Person, Passagier'
+                    ' WHERE Person.PANr = Passagier.PANr and Person.PANr = ? ', (g.user['username'],)
+                ).fetchone()['PassagierNr']
+                buchungsnummer = db.execute("Select max(Buchungsnummer) as Buchungsnummer from Buchen").fetchone()['Buchungsnummer']
+                buchungsnummer = int(buchungsnummer[1:])+1
+                buchungsnummer = "B"+str(buchungsnummer).zfill(3)
+                db.execute(
+                    'INSERT INTO Buchen (Buchungsnummer, Datum, Klasse, PassagierNr, Passagennummer)'
+                    ' VALUES (?, ?, ?, ?, ?)',
+                    (buchungsnummer, date, klasse, passpers, passage)
+                )
+                db.commit()
+                return redirect(url_for('bookings.bookings'))
+
+        else:
+            abfahrtshafen = "-- Choose a departure port --"
+            zielhafen = "-- Choose a destination port --"
+        return render_template('bookings/new_booking.html', abfahrtshafen_liste=abfahrtshafen_liste, zielhafen_liste=zielhafen_liste, abfahrtshafen=abfahrtshafen, zielhafen=zielhafen)
 
     else:
-        abfahrtshafen = "-- Wählen Sie einen Abfahrtshafen --"
-        zielhafen = "-- Wählen Sie zuerst einen Abfahrtshafen --"
-    return render_template('bookings/new_booking.html', abfahrtshafen_liste=abfahrtshafen_liste, zielhafen_liste=zielhafen_liste, abfahrtshafen=abfahrtshafen, zielhafen=zielhafen)
-
+        return render_template('non_passagier/non_passagier.html',)
